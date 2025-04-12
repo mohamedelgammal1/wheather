fef60e15d726b86255a9568dd676bf45b76755eb
Mohamed Elgammal <52075546+mohamedelgammal1@users.noreply.github.com>
2025-04-12 03:31:11 +0200

    Add Response Models and retrofit api

---
+++ app/src/main/java/com/vodafone/wheather/data/remote/ForecastResponse.kt
@@ -0,0 +1,20 @@
+package com.vodafone.wheather.data.remote
+
+data class ForecastResponse(
+    val city: CityInfo,
+    val list: List<DailyForecast>
+)
+
+data class CityInfo(
+    val name: String
+)
+
+data class DailyForecast(
+    val dt: Long,
+    val temp: TempInfo,
+    val weather: List<WeatherInfo>
+)
+
+data class TempInfo(
+    val day: Double
+)
