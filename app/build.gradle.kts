8394b1e64cd84f944be2746e367775c7adcfca94
Mohamed Elgammal <52075546+mohamedelgammal1@users.noreply.github.com>
2025-04-12 05:39:34 +0200

    Fix gradle cach issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,6 +102,8 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
+
+
     // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
